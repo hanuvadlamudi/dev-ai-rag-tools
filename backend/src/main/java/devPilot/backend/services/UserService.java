432be83133f70f5acee11c924ce49695ee4688e6
Hanumantha Rao <hanuvadlamudivhr@gmail.com>
2026-08-29 14:29:08 +0530

    add userService & methods

---
+++ backend/src/main/java/devPilot/backend/services/UserService.java
@@ -0,0 +1,40 @@
+package devPilot.backend.services;
+
+import java.util.UUID;
+
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.stereotype.Service;
+import devPilot.backend.entity.User;
+
+import devPilot.backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    
+    private final UserRepository userRepository;
+    private final TextEncryptor textEncryptor;
+
+
+
+    @Transactional()
+    public User requireById(UUID id) {
+        return userRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("User not found"));
+    }
+
+    public String decryptAccessToken(User user) {
+        return textEncryptor.decrypt(user.getAccessToken());
+    }
+
+    private static Long toLong(Object value){
+
+        if(value instanceof Number number){
+            return number.longValue();
+        }
+
+        return Long.parseLong(String.valueOf(value));
+    }
+}
